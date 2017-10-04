1b9c15cc3eca4150119520bb7044f9fa74b61591
정지원 <4dakto@gmail.com>
2017-10-04 18:55:19 +0900

    post save

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PostsIndex from './components/posts';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        start
+        <Router>
+          <div>
+            <Route
+              path="/"
+              component={PostsIndex}
+            />
+          </div>
+        </Router>
       </div>
     );
   }
